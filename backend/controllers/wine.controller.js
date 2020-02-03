c4abfe73b173006e62f55b489fb73985fdd32cf0
Rami Djema <rami.djema@mail.mcgill.ca>
2020-02-03 17:10:51 -0500

    added find all wines api endpoint

--- backend/controllers/wine.controller.js
+++ backend/controllers/wine.controller.js
@@ -1,11 +1,12 @@
 const Wine = require('../models/wine.model');
+const mongoose = require('mongoose');
 
 // test 
 exports.test = function (req, res) {
     res.send('Test controller up and running!')
 };
 
-// POST
+// POST create
 exports.wine_create = function (req, res, next) {
     let wine = new Wine(
         {
@@ -19,5 +20,37 @@ exports.wine_create = function (req, res, next) {
             return next(err);
         }
         res.send('Wine Created successfully')
+    })
+};
+
+// GET by id
+exports.wine_details = function (req, res, next) {
+    Wine.findById(req.params.id, function (err, wine) {
+        if (err) return next(err);
+        res.send(wine);
+    })
+};
+
+// PUT update by id
+exports.wine_update = function (req, res, next) {
+    Wine.findByIdAndUpdate(req.params.id, {$set: req.body}, function (err, wine) {
+        if (err) return next(err);
+        res.send('Wine Udpated.');
+    });
+};
+
+// DELETE by id
+exports.wine_delete = function (req, res, next) {
+    Wine.findByIdAndRemove(req.params.id, function (err) {
+        if (err) return next(err);
+        res.send('Deleted successfully!');
+    })
+};
+
+// GET all
+exports.wine_get_all = function (req, res) {
+    Wine.find(function (err, result) {
+        if (err) return next(err);
+        res.send(result);
     })
 };
--- backend/routes/wine.route.js
+++ backend/routes/wine.route.js
@@ -10,3 +10,15 @@ module.exports = router;
 
 // POST
 router.post('/create', wine_controller.wine_create);
+
+// GET by id
+router.get('/id/:id', wine_controller.wine_details);
+
+// GET all
+router.get('/all', wine_controller.wine_get_all);
+ 
+// PUT by id
+router.put('/:id/update', wine_controller.wine_update);
+
+// DELETE by id
+router.delete('/:id/delete', wine_controller.wine_delete);
